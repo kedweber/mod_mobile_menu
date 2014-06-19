a6d5b252a3da735fd48159d826122b8eb9402040
Jasper van Rijbroek <jasper@jvar.nl>
2014-06-19 16:28:44 +0200

    Added missing template helper.

---
+++ packages/module/mod_mobile_menu/templates/helpers/menu.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Com
+ *
+ * @author 		Joep van der Heijden <joep.van.der.heijden@moyoweb.nl>
+ * @category
+ * @package
+ * @subpackage
+ */
+
+defined('KOOWA') or die('Restricted Access');
+
+class ModMobile_menuTemplateHelperMenu extends KTemplateHelperDefault
+{
+    public function item($config = array())
+    {
+        $config = new KConfig($config);
+        $item   = $config->item;
+        $active = $config->active;
+
+        $classes = 'item-' . $item->id;
+        if ($item->id === $active->id) {
+            $classes .= ' current';
+        }
+
+        if ($item->deeper) {
+            $classes .= ' dropdown';
+        }
+
+        $classes = ' class="' . $classes . '"';
+
+        $html = '<li' . $classes . '>';
+
+        if($item->type == 'component') {
+            $html .= '<a href="' . JRoute::_('index.php?Itemid=' . $item->id) . '">' . $item->title . '</a>';
+        } else if($item->type == 'url') {
+            $html .= '<a href="'. $item->link .'" target="_blank">' . $item->title . '</a>';
+        } else if($item->type == 'separator') {
+            $html .= '<a href="#">' . $item->title . '</a>';
+        }
+
+        if ($item->deeper) {
+            $html .= '<span class="glyphicon glyphicon-plus dropdown-toggle" data-toggle="dropdown"></span>';
+            $html .= '<ul class="nav-child dropdown-menu">';
+        } else if ($item->shallower) {
+            $html .= '</ul>';
+            $html .= '<div class="clearfix"></div>';
+            $html .= '</li>';
+        } else {
+            $html .= '</li>';
+        }
+
+        return $html;
+    }
+}
